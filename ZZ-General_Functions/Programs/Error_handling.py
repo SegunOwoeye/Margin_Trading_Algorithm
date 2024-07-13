e473b2e7db0072ee5735169963a31a441d35c191
SegunOwoeye <42051567+SegunOwoeye@users.noreply.github.com>
2024-07-13 23:54:29 +0100

    Delete time file update + Error handler
    Fixed type error in error handler

--- ZZ-General_Functions/Programs/Error_handling.py
+++ ZZ-General_Functions/Programs/Error_handling.py
@@ -1,6 +1,8 @@
 ### This file handles different errors that might occur in the program
 
 from time import sleep
+import sqlite3
+
 
 class Handling_Error:
     # Initialising Error
@@ -9,18 +11,25 @@ class Handling_Error:
 
     # No data table Error handling
     def No_Data_Table_Error(self):
-        if "no such table:" in self.Error:
+        if sqlite3.OperationalError == type(self.Error):
+            # print("W")
             sleep(30) # Wait 30 seconds so the program has enough time to have deleted the file which has the error
+
         else: pass
+            #print("L")
+            
 
 
 
 """ TEST """
 
 #Variables
+'''
+try:
+    current_data("BTCUSDT", "Binance", "5m")
+except Exception as Error: 
+    print(type(Error))
 
-"""Error = "no such table in file"
-
-main = Handling_Error(Error)
-main.No_Data_Table_Error()"""
+    main = Handling_Error(Error)
+    main.No_Data_Table_Error()'''
         
