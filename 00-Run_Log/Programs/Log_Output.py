770e93d188105f8970df529b32c0689033570573
SegunOwoeye <42051567+SegunOwoeye@users.noreply.github.com>
2024-08-23 12:39:10 +0100

    Created a terminal logger, to log the events of the program

---
+++ 00-Run_Log/Programs/Log_Output.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3.8.5
+
+from datetime import datetime
+from os import path
+
+
+"""
+1. CODE FOR CREATING DATABASE AND WRITING ORDERBOOK DATA TO DATABASE
+"""
+
+
+#Generic Code - WORKING
+def creating_db_file(exchange_pair, exchange_name):
+    date_and_time = (datetime.now())
+    date = date_and_time.strftime("%b%d%y%H")
+
+    #FOR TEXT FILE 
+    file_name = f"00-Run_Log/data_gathered/" + str(date) + exchange_name + exchange_pair + "LOG.txt"
+    f = open(file_name, "w")
+
+
+def Record_Output(exchange_pair, exchange_name, Message: str):
+    date_and_time = (datetime.now())
+    date = date_and_time.strftime("%b%d%y%H")
+    file_name = f"00-Run_Log/data_gathered/" + str(date) + exchange_name + exchange_pair + "LOG.txt"
+    try:
+        #Checks to see if there's an existing db file inside the data gathering dircetory
+        if path.exists(file_name) == True:
+            
+            #Text file database
+            f = open(file_name, "a")
+            f.write(str(Message) + "\n")
+
+        else: #Creates new db file
+            creating_db_file(exchange_pair, exchange_name) #Creates new file
+
+    except Exception as e: #Message email that an error on... has occured
+        print(f"Error occured at 00-Run_Log/Programs/Log_Output.py: " + str(e))
+        
+
+
+
+#starts the program
+#Record_Output("BTCUSDT","Binance", 5)
