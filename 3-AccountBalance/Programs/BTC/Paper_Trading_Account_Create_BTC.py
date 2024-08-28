362f6a1d30d4874c5ed01eab8ea7d478489afb5d
SegunOwoeye <42051567+SegunOwoeye@users.noreply.github.com>
2024-08-28 17:26:31 +0100

    Update Paper_Trading_Account_Create_BTC.py

--- 3-AccountBalance/Programs/BTC/Paper_Trading_Account_Create_BTC.py
+++ 3-AccountBalance/Programs/BTC/Paper_Trading_Account_Create_BTC.py
@@ -1,11 +1,16 @@
 from sys import path
 
-
 path.append("3-AccountBalance/Programs")
 from Paper_Trading_Account_Create_Legacy import run
 
-#interval
-balance = 5
+# Variables
+trading_pair = "USDT"
+exchange_name = "Binance"
+db_name = "Demo_Balance"
+balance = 10
+strat_db_name = "Strategy2_Orders"
+R_Trading_Pairs = "BTCUSDT"
 
-#Run program
-run("BTC", "Binance", "Demo_Balance", balance)
+run(trading_pair, exchange_name, db_name, balance, strat_db_name, R_Trading_Pairs)
+
+                        
