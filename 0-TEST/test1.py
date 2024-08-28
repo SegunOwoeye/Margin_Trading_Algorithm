c7f41e8048ea9d55d7cb76e1665975853085441e
SegunOwoeye <42051567+SegunOwoeye@users.noreply.github.com>
2024-08-28 18:40:54 +0100

    Update test1.py

--- 0-TEST/test1.py
+++ 0-TEST/test1.py
@@ -1,6 +1,9 @@
-from sys import path 
+trading_pairs = ["BTCUSDT", "ETHUSDT", "ARBUSDT"]
 
-path.append("3-AccountBalance/Programs")
-import Paper_Trading_Account_create
+asset_pairs = []
+for i in range(len(trading_pairs)):
+    asset_name = trading_pairs[i].replace("USDT", "")
+    asset_pairs.append(asset_name)
 
-Paper_Trading_Account_create.run("USDT", "ByBit", "Demo_Balance", 10000.00)
+asset_pairs.append("USDT")
+print(asset_pairs)
