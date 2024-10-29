d3e83ca7ae94f8b597fd04fcdb6e6cdb50750ec4
SegunOwoeye <42051567+SegunOwoeye@users.noreply.github.com>
2024-10-29 21:06:02 +0000

    Asset precision and HIR update

--- 5-Trade_Monitoring/Programs/ETHUSDT/asset_precision_ETHUSDT.py
+++ 5-Trade_Monitoring/Programs/ETHUSDT/asset_precision_ETHUSDT.py
@@ -4,6 +4,6 @@ path.append("5-Trade_Monitoring/Programs")
 from asset_precision_Legacy import run
 
 # Run program
-run("BTCUSDT")
+run("ETHUSDT")
 
                         
--- 5-Trade_Monitoring/Programs/SOLUSDT/Hourly_Interest_Rate_SOLUSDT.py
+++ 5-Trade_Monitoring/Programs/SOLUSDT/Hourly_Interest_Rate_SOLUSDT.py
@@ -1,9 +1,9 @@
 from sys import path
 
 path.append("5-Trade_Monitoring/Programs")
-from asset_precision_Legacy import run
+from Hourly_Interest_Rate_Legacy import run
 
 # Run program
-run("BTCUSDT")
+run("SOLUSDT")
 
                         
