5a23684ff95a2cd36f5d0888a7402d5a65791b46
SegunOwoeye <42051567+SegunOwoeye@users.noreply.github.com>
2024-11-15 18:37:18 +0000

    Create Strategy7_Orderbook_Monitoring_BTCUSDT_Interval=1h.py

---
+++ 5-Trade_Monitoring/Programs/BTCUSDT/Strategy7_Orderbook_Monitoring_BTCUSDT_Interval=1h.py
@@ -0,0 +1,19 @@
+from sys import path
+
+
+path.append("5-Trade_Monitoring/Programs")
+from Orderbook_Monitoring_Legacy import run
+path.append("0-Settings/Program_Files/Misc/")
+from read_config import run as config # type: ignore
+
+
+#Variables
+trading_pair = "BTCUSDT"
+exchange_name = "Binance"
+flag = config()["application_settings"]['trading_environment']['flag'] # 0 LIVE | 1 DEMO
+chart_interval = "1h"
+db_name = "Strategy7_Orders"
+
+# Run Program
+run(trading_pair, exchange_name, flag, chart_interval, db_name)
+
