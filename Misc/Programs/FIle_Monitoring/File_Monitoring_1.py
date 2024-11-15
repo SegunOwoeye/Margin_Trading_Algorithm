2d59cda585213d497e920c24b5323ac866de038e
SegunOwoeye <42051567+SegunOwoeye@users.noreply.github.com>
2024-11-15 18:36:30 +0000

    Updating file monitoring
    Now pulls trading pair the config instead of being hard-coded

--- Misc/Programs/FIle_Monitoring/File_Monitoring_1.py
+++ Misc/Programs/FIle_Monitoring/File_Monitoring_1.py
@@ -2,12 +2,14 @@ from sys import path
 
 
 path.append("Misc/Programs")
+path.append("0-Settings/Program_Files/Misc/")
 from File_Monitoring_Legacy import run
+from read_config import run as config # type: ignore
 
 
 # DataGathering
 
-trading_pairs = ["BTCUSDT"] #, "ETHUSDT"]
+trading_pairs = config()["application_settings"]["pair_list"]
 
 path_list = []
 for i in range(len(trading_pairs)):
