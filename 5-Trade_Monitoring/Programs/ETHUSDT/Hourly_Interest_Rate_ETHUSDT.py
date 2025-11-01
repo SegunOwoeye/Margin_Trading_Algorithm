e661f2a98c53136a24915d3508acc0d9c72d0dda
SegunOwoeye <42051567+SegunOwoeye@users.noreply.github.com>
2025-11-01 22:22:38 +0000

    Delete 5-Trade_Monitoring/Programs/ETHUSDT directory

--- 5-Trade_Monitoring/Programs/ETHUSDT/Hourly_Interest_Rate_ETHUSDT.py
+++
@@ -1,9 +0,0 @@
-from sys import path
-
-path.append("5-Trade_Monitoring/Programs")
-from Hourly_Interest_Rate_Legacy import run
-
-# Run program
-run("ETHUSDT")
-
-                        
--- 5-Trade_Monitoring/Programs/ETHUSDT/asset_precision_ETHUSDT.py
+++
@@ -1,9 +0,0 @@
-from sys import path
-
-path.append("5-Trade_Monitoring/Programs")
-from asset_precision_Legacy import run
-
-# Run program
-run("ETHUSDT")
-
-                        
